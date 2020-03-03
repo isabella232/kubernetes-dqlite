c908e8b54811b018424d75f03573fbc0639c293c
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-03-03 12:07:48 +0100

    Vendor rancher/kine

---
+++ vendor/github.com/rancher/kine/pkg/drivers/dqlite/no_dqlite.go
@@ -0,0 +1,5 @@
+package dqlite
+
+//func New(ctx context.Context, datasourceName string) (server.Backend, error) {
+//	return nil, fmt.Errorf("dqlite is not support, compile with \"-tags dqlite\"")
+//}
